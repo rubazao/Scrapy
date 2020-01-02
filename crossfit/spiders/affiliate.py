6f788f1a2cafef02f67a47172a44e1f7be72971a
rubazao <rubazao@hotmail.com>
2020-01-02 23:59:48 +1030

    pagination added - current page+1

--- crossfit/spiders/affiliate.py
+++ crossfit/spiders/affiliate.py
@@ -5,33 +5,46 @@ import json
 
 class AffiliateSpider(scrapy.Spider):
     name = 'affiliate'
-    #allowed_domains = ['cms-api.crossfit.com']
-    page_number = 2
 
     #alternative_url = 'https://cms-api.crossfit.com/affiliates/me?page_size=420&page=2'
     # 'https://www.crossfit.com/cf/find-a-box.php?page=1'
+
+    # Initialize the first URL
+
     def start_requests(self):
-        yield scrapy.Request('https://cms-api.crossfit.com/affiliates/me?page_size=420&page=1', callback=self.parse)
+        yield scrapy.Request(url='https://cms-api.crossfit.com/affiliates/me?page_size=420&page=1',
+                             callback=self.parse,
+                             meta={
+                                 'current_page': 1
+                             }
+                             )
+    # Parse data from url (JSON / API)
 
     def parse(self, response):
         data = json.loads(response.body)
         affiliates = data.get("affiliates")
         for affiliate in affiliates:
-            yield {
-                'name': affiliate.get('name'),
-                'city': affiliate.get('city'),
-                'state': affiliate.get('full_state'),
-                'country': affiliate.get('country')
-                # 'active' : affiliate.get('active')
+            try:
+                name = affiliate.get('name')
+                city = affiliate.get('city')
+                state = affiliate.get('full_state')
+                country = affiliate.get('country')
+                yield {
+                    'name': name,
+                    'city': city,
+                    'state': state,
+                    'country': country
+                }
+            except:
+                pass
+                print("No more pages to scrape.")
+
+        # Give access to next pages of the JSON / API file
+        current_page = response.request.meta['current_page'] + 1
+        yield scrapy.Request(
+            url=f'https://cms-api.crossfit.com/affiliates/me?page_size=420&page={current_page}',
+            callback=self.parse,
+            meta={
+                'current_page': current_page
             }
-
-        # if 'name' == 50:
-        #     page = 1
-        #     for i in range(1000000):
-        #         page = i+1
-
-        next_page = 'https://cms-api.crossfit.com/affiliates/me?page_size=420&page=' + \
-            str(AffiliateSpider.page_number)
-        if AffiliateSpider.page_number < 25:
-            AffiliateSpider.page_number += 1
-            yield scrapy.Request(next_page, callback=self.parse)
+        )
